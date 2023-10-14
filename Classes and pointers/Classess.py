9906e86d901c937621abf9ad03126504e7ae0649
jawad-nsu <jawadurrahim1997@gmail.com>
2023-10-13 21:47:14 -0400

    classes in python

---
+++ Classes and pointers/Classess.py
@@ -0,0 +1,23 @@
+class Cookie:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+    
+    def set_color(self, color):
+        self.color = color
+
+
+
+cookie_one = Cookie('green')
+cookie_two = Cookie('blue')
+
+print(cookie_one.get_color())
+print(cookie_two.get_color())
+
+cookie_one.set_color('purple')
+cookie_two.set_color('orange')
+
+print(cookie_one.get_color())
+print(cookie_two.get_color())
