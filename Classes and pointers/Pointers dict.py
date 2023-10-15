661c4666516696802ceac00287547826e50d77e6
jawad-nsu <jawadurrahim1997@gmail.com>
2023-10-14 21:15:25 -0400

    let's print the value & address of dict1 & dict2

--- Classes and pointers/Pointers dict.py
+++ Classes and pointers/Pointers dict.py
@@ -1,4 +1,15 @@
 dict1 = {'value': 11}
 dict2 = dict1
 
+print('before dict2 is updated')
+print('value of dict1: ', dict1)
+print('value of dict2: ', dict2)
+print('address of dict1: ', id(dict1))
+print('address of dict2: ', id(dict2))
+
 dict2['value'] = 22
+print('\nafter updating dict2')
+print('value of dict1: ', dict1)
+print('value of dict2: ', dict2)
+print('address of dict1: ', id(dict1))
+print('address of dict2: ', id(dict2))
