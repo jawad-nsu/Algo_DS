320a0364163fdc0d6ceef654150e8a2cda9cecf5
jawad-nsu <jawadurrahim1997@gmail.com>
2023-10-14 21:47:15 -0400

    create new dict3, point dict2 to dict3

--- Classes and pointers/Pointers dict.py
+++ Classes and pointers/Pointers dict.py
@@ -1,7 +1,7 @@
 dict1 = {'value': 11}
 dict2 = dict1
 
-print('before dict2 is updated')
+# print('before dict2 is updated')
 print('value of dict1: ', dict1)
 print('value of dict2: ', dict2)
 print('address of dict1: ', id(dict1))
@@ -12,4 +12,9 @@ print('\nafter updating dict2')
 print('value of dict1: ', dict1)
 print('value of dict2: ', dict2)
 print('address of dict1: ', id(dict1))
+print('address of dict2: ', id(dict2))
+
+dict3 = {'value': 33}
+dict2 = dict3
+print('\nvalue of dict2: ', dict2)
 print('address of dict2: ', id(dict2))
