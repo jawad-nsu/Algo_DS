5d6e261780e373562a4b404e6364c3ba50745010
jawad-nsu <jawadurrahim1997@gmail.com>
2024-03-19 14:56:46 -0400

    DLL: append

--- Doublely Linked List/append.py
+++ Doublely Linked List/append.py
@@ -27,5 +27,17 @@ class DoublelyLinkedList:
             new_node.next = self.tail
             new_node.prev = self.head
         else: 
-            
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+        self.length += 1
+        return True
         
+
+DLL = DoublelyLinkedList(1)
+DLL.append(2)
+DLL.append(3)
+DLL.append(4)
+DLL.append(5)
+DLL.print_list()
