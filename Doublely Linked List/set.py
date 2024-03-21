2a58360ca099eba21c05bf6ba3dd08a8f7ad409c
jawad-nsu <jawadurrahim1997@gmail.com>
2024-03-21 11:17:53 -0400

    DLL set value: reuse get method instead of code duplication

--- Doublely Linked List/set.py
+++ Doublely Linked List/set.py
@@ -29,20 +29,6 @@ class DoublyLinkedList:
             self.tail = new_node
         self.length += 1
         return True
-    
-    def prepend(self, value):
-        new_node = Node(value)
-
-        if self.length < 1:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-
-        self.length += 1
-        return True
 
     def pop(self):
         if self.length == 0:
@@ -57,80 +43,84 @@ class DoublyLinkedList:
             temp.prev = None
         self.length -= 1
         return temp
-    
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
     def pop_first(self):
-        if self.length < 1:
+        if self.length == 0:
             return None
-        
-        temp = self.head 
+        temp = self.head
         if self.length == 1:
             self.head = None
             self.tail = None
         else:
-            self.head = temp.next
+            self.head = self.head.next
             self.head.prev = None
-            temp.next = None
-
+            temp.next = None      
         self.length -= 1
         return temp
-    
+
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
-        
+        temp = self.head
         if index < self.length/2:
-            curr = self.head
             for _ in range(index):
-                curr = curr.next
-            return curr
+                temp = temp.next
         else:
-            curr = self.tail
-            for _ in range(self.length-1, index, -1):
-                curr = curr.prev
-            return curr
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev  
+        return temp
         
     def set_value(self, index, value):
-        if index < 0 or index >= self.length:
-            return None
-        
-        if index < self.length/2:
-            curr = self.head
-            for _ in range(index):
-                curr = curr.next
-            curr.value = value
-            return curr
-        else:
-            curr = self.tail
-            for _ in range(self.length-1, index, -1):
-                curr = curr.prev
-            curr.value = value
-            return curr
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
 
-my_doubly_linked_list = DoublyLinkedList(1)
-my_doubly_linked_list.append(2)
+    
+  
+my_doubly_linked_list = DoublyLinkedList(11)
 my_doubly_linked_list.append(3)
-my_doubly_linked_list.append(4)
-my_doubly_linked_list.print_list()
-print('get', my_doubly_linked_list.get(0).value)
-print('get', my_doubly_linked_list.get(1).value)
-print('get', my_doubly_linked_list.get(2).value)
-print('get', my_doubly_linked_list.get(3).value)
+my_doubly_linked_list.append(23)
+my_doubly_linked_list.append(7)
 
-my_doubly_linked_list.set_value(0, 100)
-my_doubly_linked_list.set_value(1, 200)
-my_doubly_linked_list.set_value(2, 300)
-my_doubly_linked_list.set_value(3, 400)
+print('DLL before set_value():')
 my_doubly_linked_list.print_list()
+
+my_doubly_linked_list.set_value(1,4)
+
+print('\nDLL after set_value():')
+my_doubly_linked_list.print_list()
+
+
 
 """
     EXPECTED OUTPUT:
     ----------------
-    Head: 1
-    Tail: 2
-    Length: 2 
+    DLL before set_value():
+    11
+    3
+    23
+    7
 
-    Doubly Linked List:
-    1
-    2
-    
+    DLL after set_value():
+    11
+    4
+    23
+    7
+
 """
