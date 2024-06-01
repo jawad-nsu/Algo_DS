135ab6484b6a15646fdd40d38087a4b14ea31eb6
jawad-nsu <jawadurrahim1997@gmail.com>
2024-05-31 21:49:40 -0400

    Stack: push on top method

---
+++ Stack/push.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.length = 1
+    
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.length += 1
+
+my_stack = Stack(4)
+
+my_stack.print_stack()
+
+my_stack.push(3)
+
+my_stack.print_stack()
