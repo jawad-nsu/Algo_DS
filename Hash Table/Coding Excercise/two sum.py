543ac0048442badfd75de1afb08d641f6f78f873
jawad-nsu <jawadurrahim1997@gmail.com>
2025-01-02 23:44:39 -0500

    two sum using hash table

---
+++ Hash Table/Coding Excercise/two sum.py
@@ -0,0 +1,42 @@
+def two_sum(arr, target):
+    if len(arr) == 0:
+        return []
+        
+    my_map = {}
+    
+    for i in range(len(arr)):
+        res = target - arr[i]
+        if res in my_map:
+            return [i, my_map[res]]
+        else:
+            my_map[arr[i]] = i
+    return []
+    
+    
+    
+print(two_sum([5, 1, 7, 2, 9, 3], 10))  
+print(two_sum([4, 2, 11, 7, 6, 3], 9))  
+print(two_sum([10, 15, 5, 2, 8, 1, 7], 12))  
+print(two_sum([1, 3, 5, 7, 9], 10))  
+print ( two_sum([1, 2, 3, 4, 5], 10) )
+print ( two_sum([1, 2, 3, 4, 5], 7) )
+print ( two_sum([1, 2, 3, 4, 5], 3) )
+print ( two_sum([], 0) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 4]
+    [1, 3]
+    [0, 3]
+    [1, 3]
+    []
+    [2, 3]
+    [0, 1]
+    []
+
+"""
+
+
